647dc14b4fa2d36aee6d3176a53797ad7897a1e0
Tamara Oweibo <oweibor@gmail.com>
2026-03-02 07:44:01 +0100

    fix(checkpoint): fix incorrect require path for logger module

--- kilo/pipeline/src/services/scraper/langgraph/checkpoint.js
+++ kilo/pipeline/src/services/scraper/langgraph/checkpoint.js
@@ -10,7 +10,7 @@
 
 const fs = require('fs').promises;
 const path = require('path');
-const logger = require('../logger');
+const logger = require('../../logger');
 
 /**
  * File-based checkpoint store.
